1f37cfb0c0bbba3a660bac6a0d0968bd738b6619
Zarpec <axelza25@gmail.com>
2025-01-05 12:45:05 -0600

    se implementa el enrutamiento con React Router y configurar rutas iniciales

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import PagPrueba from "./pages/Pueba.js"
+import LoginForm from "./pages/login.js"
+import Categorias from "./pages/productos/categorias.js";
+import Productos from "./pages/productos/catalogo.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PagPrueba />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/categorias" element={<Categorias />} />
+        <Route path="/productos" element={<Productos/>} />
+      </Routes>
+    </Router>
   );
 }
 
