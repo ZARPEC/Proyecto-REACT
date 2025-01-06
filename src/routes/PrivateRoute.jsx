a8d2675d08e68859d00800e630d3b284678441d3
Zarpec <axelza25@gmail.com>
2025-01-06 02:54:40 -0600

    se actualiza el componente PrivateRoute para incluir verificación de roles de usuario

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,14 +2,19 @@ import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
-const PrivateRoute = ({ children }) => {
-  const { isAuthenticated, isLoading } = useContext(AuthContext);
+const PrivateRoute = ({ children, requiredRole }) => {
+  const { isAuthenticated, role, isLoading } = useContext(AuthContext);
 
   if (isLoading) {
-    return <div>Cargando...</div>; // Muestra un indicador mientras se verifica la autenticación
+    return <div>Cargando...</div>;
   }
 
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  // Si el usuario no está autenticado o no tiene el rol adecuado
+  if (!isAuthenticated || (requiredRole && requiredRole !== role)) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
 };
 
 export default PrivateRoute;
