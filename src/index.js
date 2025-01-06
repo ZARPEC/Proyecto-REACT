5e857a1c4968d56be18233f3f6fe9c36e33936d6
Zarpec <axelza25@gmail.com>
2025-01-05 23:34:00 -0600

    se integra el contexto de autenticación en el componente Navbar

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import Navbar from "./components/navbar";
+import { AuthProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-const navbarRoot = ReactDOM.createRoot(document.getElementById('navbar'));
+const navbarRoot = ReactDOM.createRoot(document.getElementById("navbar"));
 navbarRoot.render(
-  <React.StrictMode>
-    <Navbar />
-  </React.StrictMode>
+  <AuthProvider>
+    <React.StrictMode>
+      <Navbar />
+    </React.StrictMode>
+  </AuthProvider>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
