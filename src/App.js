9120f63844ccd1d50795bb0fb8713404b6bcf888
Zarpec <axelza25@gmail.com>
2025-01-05 23:33:51 -0600

    se integra el contexto de autenticación y se implementa la protección de rutas en el componente App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import PagPrueba from "./pages/Pueba.js"
-import LoginForm from "./pages/login.js"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PagPrueba from "./pages/Pueba.js";
+import LoginForm from "./pages/login.js";
 import Categorias from "./pages/productos/categorias.js";
 import Productos from "./pages/productos/catalogo.js";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import PrivateRoute from "./routes/PrivateRoute.jsx";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PagPrueba />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/categorias" element={<Categorias />} />
-        <Route path="/productos" element={<Productos/>} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <PagPrueba />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/categorias" element={<Categorias />} />
+          <Route path="/productos" element={<Productos />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
