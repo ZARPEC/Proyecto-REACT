5dfb746d8c96f81903627bc3a016363f1b745200
Zarpec <axelza25@gmail.com>
2025-01-07 22:20:29 -0600

    se añaden pruebas unitarias para el componente CrudProductos

--- src/pages/usuarios/crudUsuarios.js
+++ src/pages/usuarios/crudUsuarios.js
@@ -14,31 +14,28 @@ import {
   DialogActions,
   TextField,
   IconButton,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import { Edit as EditIcon, Delete as DeleteIcon } from "@mui/icons-material";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import RestoreIcon from "@mui/icons-material/Restore";
+import { useForm, Controller } from "react-hook-form";
 
 function CrudUsuarios() {
   const [users, setUsers] = useState([]);
+  const [roles, setRoles] = useState([]);
+  const [showPassword, setShowPassword] = useState(false);
   const token = localStorage.getItem("token");
   const [open, setOpen] = useState(false);
   const [editOpen, setEditOpen] = useState(false);
   const [currentUser, setCurrentUser] = useState(null);
   const [filtro, setFiltro] = useState("activo");
-  const [formData, setFormData] = useState({
 
-    rol: "",
-    estado: "",
-    email: "",
-    pass: "",
-    nombre: "",
-    apellido: "",
-    telefono: "",
-    nacimiento: "",
-  });
-
-  // Manejo de apertura y cierre de diálogos
-  const handleClickOpen = () => {
-    setFormData({
+  const { handleSubmit, control, reset } = useForm({
+    defaultValues: {
       idUsuario: "",
       rol: "",
       estado: "",
@@ -48,7 +45,11 @@ function CrudUsuarios() {
       apellido: "",
       telefono: "",
       nacimiento: "",
-    });
+    },
+  });
+  // Manejo de apertura y cierre de diálogos
+  const handleClickOpen = () => {
+   reset();
     setOpen(true);
   };
 
@@ -57,20 +58,13 @@ function CrudUsuarios() {
     setEditOpen(false);
   };
 
-  // Actualizar el formulario
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-  const handlefiltro =(filtro)=>{
+  
+  const handlefiltro = (filtro) => {
     setFiltro(filtro);
-  }
+  };
 
   // Crear o editar un usuario
-  const handleSubmit = async () => {
+  const onSubmit = async (data) => {
     try {
       const url = editOpen
         ? "http://localhost:3005/usuario/modificarUsuario"
@@ -82,34 +76,22 @@ function CrudUsuarios() {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          idUsuario: formData.idUsuario,
-          rol: formData.rol,
-          estado: formData.estado,
-          email: formData.email,
-          pass: formData.pass,
-          nombre: formData.nombre,
-          apellido: formData.apellido,
-          telefono: formData.telefono,
-          nacimiento: formData.nacimiento,
-        }),
+        body: JSON.stringify(data),
       });
 
       if (!response.ok) {
         throw new Error(`Error al ${editOpen ? "editar" : "crear"} usuario.`);
       }
 
-      console.log(formData);
       const updatedUser = await response.json();
-      const nuevoUsuario = { ...formData, idUsuario: updatedUser.id };
       if (editOpen) {
         setUsers(
           users.map((user) =>
-            user.idUsuario === updatedUser.idUsuario ? nuevoUsuario : user
+            user.idUsuario === updatedUser.idUsuario ? updatedUser : user
           )
         );
       } else {
-        setUsers([...users, nuevoUsuario]);
+        setUsers([...users, updatedUser]);
       }
       handleClose();
     } catch (error) {
@@ -119,6 +101,7 @@ function CrudUsuarios() {
 
   // Eliminar un usuario
   const handleDelete = async (idUsuario) => {
+    var estadoSet = filtro == "activo" ? 3 : 1;
     try {
       const url = `http://localhost:3005/cliente/modificarEstadoCliente`;
       const response = await fetch(url, {
@@ -127,7 +110,7 @@ function CrudUsuarios() {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ idCliente: idUsuario, estado_fk: 3 }),
+        body: JSON.stringify({ idCliente: idUsuario, estado_fk: estadoSet }),
       });
 
       if (!response.ok) {
@@ -142,34 +125,61 @@ function CrudUsuarios() {
 
   // Cargar usuarios desde la API
   useEffect(() => {
-    const fetchUsers = async () => {
-      var url="";
-      if(filtro==="activo"){
+    if (editOpen && currentUser) {
+      reset({
+        idUsuario: currentUser.idUsuario,
+        rol: currentUser.rol,
+        estado: currentUser.estado,
+        email: currentUser.email,
+        pass: currentUser.pass,
+        nombre: currentUser.nombre,
+        apellido: currentUser.apellido,
+        telefono: currentUser.telefono,
+        nacimiento: currentUser.nacimiento,
+      });
+    }
+    const fetchData = async () => {
+      var url = "";
+      if (filtro === "activo") {
         url = "http://localhost:3005/cliente/mostrarClientes";
-      }else if(filtro==="inactivo"){
+      } else if (filtro === "inactivo") {
         url = "http://localhost:3005/cliente/mostrarClientes?estado=Inactivos";
       }
       try {
-        const response = await fetch(url, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
+        const [responseClientes, responseRol] = await Promise.all([
+          fetch(url, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }),
+          fetch("http://localhost:3005/roles/mostrarRoles", {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }),
+        ]);
 
-        if (!response.ok) {
+        if (!responseClientes.ok) {
           throw new Error("Error al cargar usuarios.");
         }
+        if (!responseRol.ok) {
+          throw new Error("Error al cargar roles.");
+        }
 
-        const data = await response.json();
+        const data = await responseClientes.json();
+        const datarol = await responseRol.json();
         setUsers(data);
+        setRoles(datarol);
       } catch (error) {
         console.error("Error al cargar usuarios:", error);
       }
     };
-    fetchUsers();
-  }, [token,filtro]);
+    fetchData();
+  }, [token, filtro, currentUser,editOpen]);
 
   return (
     <div
@@ -178,7 +188,6 @@ function CrudUsuarios() {
         width: "100%",
         display: "flex",
         justifyContent: "center",
-
       }}
     >
       <div style={{ width: "75" }}>
@@ -186,22 +195,26 @@ function CrudUsuarios() {
           variant="contained"
           color="primary"
           onClick={handleClickOpen}
-          style={{ marginBottom: "20px",marginRight: "20px"  }}
+          style={{ marginBottom: "20px", marginRight: "20px" }}
         >
           Crear Nuevo Usuario
-        </Button >
-        <Button variant="contained"
+        </Button>
+        <Button
+          variant="contained"
           color="primary"
           onClick={() => handlefiltro("inactivo")}
-          style={{ marginBottom: "20px",marginRight: "20px" }}>
-            Usuarios Inactivos
-          </Button>
-        <Button variant="contained"
+          style={{ marginBottom: "20px", marginRight: "20px" }}
+        >
+          Usuarios Inactivos
+        </Button>
+        <Button
+          variant="contained"
           color="primary"
           onClick={() => handlefiltro("activo")}
-          style={{ marginBottom: "20px",marginRight: "20px"  }}>
-            Usuarios Activos
-          </Button>
+          style={{ marginBottom: "20px", marginRight: "20px" }}
+        >
+          Usuarios Activos
+        </Button>
 
         <TableContainer component={Paper}>
           <Table>
@@ -223,19 +236,27 @@ function CrudUsuarios() {
                     <IconButton
                       onClick={() => {
                         setCurrentUser(user);
-                        setFormData(user);
                         setEditOpen(true);
                       }}
                       color="primary"
                     >
                       <EditIcon />
                     </IconButton>
-                    <IconButton
-                      onClick={() => handleDelete(user.idUsuario)}
-                      color="error"
-                    >
-                      <DeleteIcon />
-                    </IconButton>
+                    {filtro === "activo" ? (
+                      <IconButton
+                        onClick={() => handleDelete(user.idUsuario)}
+                        color="error"
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    ) : (
+                      <IconButton
+                        onClick={() => handleDelete(user.idUsuario)}
+                        color="success"
+                      >
+                        <RestoreIcon />
+                      </IconButton>
+                    )}
                   </TableCell>
                   <TableCell>{user.nombre}</TableCell>
                   <TableCell>{user.apellido}</TableCell>
@@ -251,47 +272,83 @@ function CrudUsuarios() {
 
         {/* Diálogo para crear/editar usuarios */}
         <Dialog open={open || editOpen} onClose={handleClose}>
-          <DialogTitle>
-            {editOpen ? "Editar Usuario" : "Crear Usuario"}
-          </DialogTitle>
-          <DialogContent>
-            {[
-              "nombre",
-              "apellido",
-              "email",
-              "pass",
-              "telefono",
-              "rol",
-              "estado",
-              "nacimiento",
-            ].map((field) => (
-              <TextField
-                key={field}
-                margin="dense"
-                name={field}
-                label={field.charAt(0).toUpperCase() + field.slice(1)}
-                type={
-                  field === "nacimiento"
-                    ? "date"
-                    : field === "rol" || field === "estado"
-                    ? "number"
-                    : "text"
-                }
-                fullWidth
-                value={formData[field] || ""}
-                onChange={handleInputChange}
-              />
-            ))}
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              Cancelar
-            </Button>
-            <Button onClick={handleSubmit} color="primary">
-              {editOpen ? "Guardar" : "Crear"}
-            </Button>
-          </DialogActions>
-        </Dialog>
+        <DialogTitle>{editOpen ? "Editar Usuario" : "Crear Usuario"}</DialogTitle>
+        <DialogContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name="nombre"
+              control={control}
+              render={({ field }) => (
+                <TextField {...field} label="Nombre" fullWidth />
+              )}
+            />
+            <Controller
+              name="apellido"
+              control={control}
+              render={({ field }) => (
+                <TextField {...field} label="Apellido" fullWidth />
+              )}
+            />
+            <Controller
+              name="email"
+              control={control}
+              render={({ field }) => (
+                <TextField {...field} label="Email" fullWidth/>
+              )}
+            />
+            <Controller
+              name="pass"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Contraseña"
+                  type={showPassword ? "text" : "password"}
+                  fullWidth
+                />
+              )}
+            />
+            <Controller
+              name="telefono"
+              control={control}
+              render={({ field }) => (
+                <TextField {...field} label="Teléfono" fullWidth/>
+              )}
+            />
+            <Controller
+              name="rol"
+              control={control}
+              render={({ field }) => (
+                <Select {...field} fullWidth>
+                  {roles.map((rol) => (
+                    <MenuItem key={rol.idRol} value={rol.idRol}>
+                      {rol.nombreRol}
+                    </MenuItem>
+                  ))}
+                </Select>
+              )}
+            />
+            <Controller
+              name="nacimiento"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Fecha de Nacimiento"
+                  type="date"
+                  fullWidth
+                />
+              )}
+            />
+            <DialogActions>
+              <Button onClick={handleClose}>Cancelar</Button>
+              <Button type="submit" color="primary">
+                {editOpen ? "Guardar" : "Crear"}
+              </Button>
+            </DialogActions>
+          </form>
+        </DialogContent>
+      </Dialog>
       </div>
     </div>
   );
