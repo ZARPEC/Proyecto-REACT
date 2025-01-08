4ebda5c9dc0972822749596a97e86e4ecdf94640
Zarpec <axelza25@gmail.com>
2025-01-08 03:44:12 -0600

    se refactoriza el componente CrudOrdenes para incluir gestión de usuarios y filtros de estado de órdenes

--- src/pages/ordenesCrud.js
+++ src/pages/ordenesCrud.js
@@ -15,43 +15,54 @@ import {
   TableRow,
   Paper,
   IconButton,
+  MenuItem,
 } from "@mui/material";
 import { Edit as EditIcon, Delete as DeleteIcon } from "@mui/icons-material";
+import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
+const initialFormState = {
+  idorden: "",
+  usuario: "",
+  direccion: "",
+};
 
-function CrudOrdenes() {
+const CrudOrdenes = () => {
   const [ordenes, setOrdenes] = useState([]);
   const [dialogOpen, setDialogOpen] = useState(false);
   const [editMode, setEditMode] = useState(false);
-  const [selectedOrden, setSelectedOrden] = useState(null);
-  const [formData, setFormData] = useState({
-    idorden: "",
-    nombre: "",
-    apellido: "",
-    email: "",
-    nombre_Comercial: "",
-    direccion: "",
-    nombre_producto: "",
-    cantidad_medida: "",
-    cantidad: "",
-    precio: "",
-    total: "",
-    fecha_orden: "",
-    nombreEstado: "",
-  });
+  const [formData, setFormData] = useState(initialFormState);
+  const [usuarios, setUsuario] = useState([]);
+  const [filtro, setFiltro] = useState("pendiente");
 
   const token = localStorage.getItem("token");
 
   useEffect(() => {
     fetchOrdenes();
-  }, []);
+  }, [filtro,token]);
+
+  useEffect(() => {
+    const fetchUsuarios = async () => {
+      try {
+        const responseUsuario = await axios.get(
+          "http://localhost:3005/cliente/mostrarClientes",
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        console.log(responseUsuario.data);
+        setUsuario(responseUsuario.data);
+      } catch (error) {
+        console.error("Error al cargar los usuarios:", error);
+      }
+    };
+    fetchUsuarios();
+  }, [token]);
 
   const fetchOrdenes = async () => {
+    
     try {
       const response = await axios.get(
-        "http://localhost:3005/ordenes/mostrarOrdenes",
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
+        filtro === "entregada"?
+        "http://localhost:3005/ordenes/mostrarOrdenes":
+        "http://localhost:3005/ordenes/mostrarOrdenesPendientes",
+        { headers: { Authorization: `Bearer ${token}` } }
       );
       setOrdenes(response.data);
     } catch (error) {
@@ -59,44 +70,47 @@ function CrudOrdenes() {
     }
   };
 
+  const handleFiltro = (nuevoFiltro) => {
+    setFiltro(nuevoFiltro);
+  };
+
+  const handleDelete = async (idorden) => {
+    const estado = filtro === "entregada" ? 4 : 7;
+    try {
+      await axios.post(
+        "http://localhost:3005/ordenes/modificarEstadoOrden",
+        { idorden, estado_fk: estado },
+        {
+             headers: { Authorization: `Bearer ${token}` },}
+      );
+
+      fetchOrdenes();
+    } catch (error) {
+      console.error("Error al eliminar la orden:",error.response|| error);
+    }
+  };
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
   };
 
   const handleOpenDialog = (orden = null) => {
     setEditMode(!!orden);
-    setSelectedOrden(orden);
-    setFormData(
-      orden || {
-        idorden: "",
-        nombre: "",
-        apellido: "",
-        email: "",
-        nombre_Comercial: "",
-        direccion: "",
-        nombre_producto: "",
-        cantidad_medida: "",
-        cantidad: "",
-        precio: "",
-        total: "",
-        fecha_orden: "",
-        nombreEstado: "",
-      }
-    );
+    setFormData(orden || initialFormState);
     setDialogOpen(true);
   };
 
   const handleCloseDialog = () => {
     setDialogOpen(false);
-    setSelectedOrden(null);
+    setFormData(initialFormState);
   };
 
   const handleSubmit = async () => {
     try {
       if (editMode) {
-        await axios.put(
-          `http://127.0.0.1:82/minisuper/api/ordenes/${selectedOrden.idorden}`,
+        await axios.post(
+          `http://localhost:3005/ordenes/modificarOrden`,
           formData,
           { headers: { Authorization: `Bearer ${token}` } }
         );
@@ -116,32 +130,26 @@ function CrudOrdenes() {
     }
   };
 
-  const handleDelete = async (idorden) => {
-    if (!window.confirm("¿Estás seguro de que quieres eliminar esta orden?"))
-      return;
-    try {
-      await axios.delete(
-        `http://127.0.0.1:82/minisuper/api/ordenes/${idorden}`,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-      fetchOrdenes();
-    } catch (error) {
-      console.error("Error al eliminar la orden:", error);
-    }
-  };
-
   return (
     <div style={{ padding: "20px" }}>
       <h1>Gestión de Órdenes</h1>
       <Button
         variant="contained"
-        color="primary"
-        onClick={() => handleOpenDialog()}
+        color="secondary"
+        onClick={() => handleFiltro("pendiente")}
+        style={{ marginBottom: "20px", marginRight: "20px" }}
       >
-        Crear Orden
+        Ordenes Pendientes
       </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => handleFiltro("entregada")}
+        style={{ marginBottom: "20px", marginRight: "20px" }}
+      >
+        Ordenes Entregadas
+      </Button>
+
       <TableContainer component={Paper} style={{ marginTop: "20px" }}>
         <Table>
           <TableHead>
@@ -150,8 +158,6 @@ function CrudOrdenes() {
               <TableCell>ID</TableCell>
               <TableCell>Cliente</TableCell>
               <TableCell>Producto</TableCell>
-              <TableCell>Cantidad</TableCell>
-              <TableCell>Precio</TableCell>
               <TableCell>Total</TableCell>
               <TableCell>Estado</TableCell>
               <TableCell>Fecha</TableCell>
@@ -162,26 +168,22 @@ function CrudOrdenes() {
               <TableRow key={orden.idorden}>
                 <TableCell>
                   <IconButton
-                    variant="outlined"
-                    color="primary"
                     onClick={() => handleOpenDialog(orden)}
                     style={{ marginRight: "10px" }}
                   >
                     <EditIcon color="primary" />
                   </IconButton>
-                  <IconButton
-                    variant="outlined"
-                    color="secondary"
-                    onClick={() => handleDelete(orden.idorden)}
-                  >
-                    <DeleteIcon color="error" />
+                  <IconButton onClick={() => handleDelete(orden.idorden)}>
+                    {filtro === "entregada" ? (
+                      <DeleteIcon color="error" />
+                    ) : (
+                      <DoneOutlineIcon color="success" />
+                    )}
                   </IconButton>
                 </TableCell>
                 <TableCell>{orden.idorden}</TableCell>
                 <TableCell>{`${orden.nombre} ${orden.apellido}`}</TableCell>
-                <TableCell>{orden.nombre_producto}</TableCell>
-                <TableCell>{orden.cantidad_medida}</TableCell>
-                <TableCell>{orden.precio}</TableCell>
+                <TableCell>{orden.productos}</TableCell>
                 <TableCell>{orden.total}</TableCell>
                 <TableCell>{orden.nombreEstado}</TableCell>
                 <TableCell>
@@ -196,65 +198,48 @@ function CrudOrdenes() {
       <Dialog open={dialogOpen} onClose={handleCloseDialog}>
         <DialogTitle>{editMode ? "Editar Orden" : "Crear Orden"}</DialogTitle>
         <DialogContent>
+          {Object.keys(initialFormState).map(
+            (field) =>
+              field !== "idorden" &&
+              field !== "usuario" && (
+                <TextField
+                  key={field}
+                  name={field}
+                  label={field.replace("_", " ").toUpperCase()}
+                  value={formData[field] || ""}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="dense"
+                  type={
+                    field === "cantidad" ||
+                    field === "precio" ||
+                    field === "total"
+                      ? "number"
+                      : "text"
+                  }
+                />
+              )
+          )}
           <TextField
-            name="nombre"
-            label="Nombre"
-            value={formData.nombre}
+            select
+            label="USUARIO"
+            name="usuario"
+            value={formData.usuario || ""}
             onChange={handleInputChange}
             fullWidth
             margin="dense"
-          />
-          <TextField
-            name="apellido"
-            label="Apellido"
-            value={formData.apellido}
-            onChange={handleInputChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            name="email"
-            label="Correo"
-            value={formData.email}
-            onChange={handleInputChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            name="nombre_producto"
-            label="Producto"
-            value={formData.nombre_producto}
-            onChange={handleInputChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            name="cantidad"
-            label="Cantidad"
-            type="number"
-            value={formData.cantidad}
-            onChange={handleInputChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            name="precio"
-            label="Precio"
-            type="number"
-            value={formData.precio}
-            onChange={handleInputChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            name="total"
-            label="Total"
-            type="number"
-            value={formData.total}
-            onChange={handleInputChange}
-            fullWidth
-            margin="dense"
-          />
+          >
+            {/* Verificamos si 'usuarios' está definido y es un array */}
+            {Array.isArray(usuarios) && usuarios.length > 0 ? (
+              usuarios.map((usuario) => (
+                <MenuItem key={usuario.idUsuario} value={usuario.idUsuario}>
+                  {usuario.email}
+                </MenuItem>
+              ))
+            ) : (
+              <MenuItem disabled>No hay usuarios disponibles</MenuItem>
+            )}
+          </TextField>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCloseDialog} color="secondary">
@@ -267,6 +252,6 @@ function CrudOrdenes() {
       </Dialog>
     </div>
   );
-}
+};
 
 export default CrudOrdenes;
