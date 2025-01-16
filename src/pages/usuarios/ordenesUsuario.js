0187ee4b552f273e95dac6d03e99198ec7665bbe
Zarpec <axelza25@gmail.com>
2025-01-16 00:27:43 -0600

    se añade un diálogo para mostrar detalles de la orden en el componente OrdenesUsuario, se optimiza la lógica de filtrado de órdenes y se mejora la gestión de estados

--- src/pages/usuarios/ordenesUsuario.js
+++ src/pages/usuarios/ordenesUsuario.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   Table,
   TableBody,
   TableCell,
@@ -11,13 +15,29 @@ import {
   TablePagination,
   Chip,
   Box,
+  Button,
 } from "@mui/material";
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 
 const OrdenesUsuario = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [ordenes, setOrdenes] = useState([]);
+  const [dialogOpenDetalles, setDialogOpenDetalles] = useState(false);
+  const [idOrden, setIdOrden] = useState(0);
+  const ordenesUnicas = ordenes.filter(
+    (orden, index, self) =>
+      index === self.findIndex((o) => o.idorden === orden.idorden)
+  );
 
+  const handleCloseDialogDetalles = () => {
+    setDialogOpenDetalles(false);
+  };
+
+  const handleOpenDialogDetalles = (idOrden) => {
+    setDialogOpenDetalles(true);
+    setIdOrden(idOrden);
+  };
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -28,40 +48,43 @@ const OrdenesUsuario = () => {
     setPage(0);
   };
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        const idCLiente = localStorage.getItem("idUsuario");
-        const fetchOrdenes = async () => {
-            try {
-                const response = await fetch(`http://localhost:3005/ordenes/mostrarOrdenes?idCliente=${idCLiente}`, {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
-                const data = await response.json();
-                setOrdenes(data);
-                console.log(data);
-                if(!response.ok){
-                    throw new Error(data.message);
-                }
-            } catch (error) {
-                console.error("Error:", error);
-            }
-        };
-        fetchOrdenes();
-    },[]);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const idCLiente = localStorage.getItem("idUsuario");
+    const fetchOrdenes = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3005/ordenes/mostrarOrdenes?idCliente=${idCLiente}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data = await response.json();
+        setOrdenes(data);
+        console.log(data);
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchOrdenes();
+  }, []);
 
   const getStatusColor = (status) => {
     switch (status) {
       case "entregado":
         return "success";
-      case "pendiente":
+      case "Pendiente":
         return "warning";
       case "Enviado":
         return "info";
-      case "Cancelado":
+      case "Rechazado":
         return "error";
       default:
         return "default";
@@ -70,7 +93,7 @@ const OrdenesUsuario = () => {
 
   return (
     <div
-    style={{
+      style={{
         padding: "20px",
         width: "100%",
         display: "flex",
@@ -87,30 +110,33 @@ const OrdenesUsuario = () => {
               <TableHead>
                 <TableRow>
                   <TableCell>ID Orden</TableCell>
-                  <TableCell>Productos</TableCell>
-                  <TableCell align="right">Total</TableCell>
+                  <TableCell>Detalles</TableCell>
                   <TableCell>Fecha</TableCell>
                   <TableCell>Estado</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {ordenes
+                {ordenesUnicas
                   .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                   .map((order) => (
-                    <TableRow key={order.idorden
-                    }>
+                    <TableRow key={order.idorden}>
                       <TableCell component="th" scope="row">
                         {order.idorden}
                       </TableCell>
-                      <TableCell>{order.productos}</TableCell>
-                      <TableCell align="right">
-                        Q{order.total.toFixed(2)}
-                      </TableCell>
+                      <TableCell>
+                  <Button
+                    onClick={() => handleOpenDialogDetalles(order.idorden)}
+                  >
+                    <ReceiptLongIcon />
+                    Detalles
+                  </Button>
+                </TableCell>
+                     
                       <TableCell>{order.fecha_orden}</TableCell>
                       <TableCell>
                         <Chip
                           label={order.nombreEstado}
-                          color={getStatusColor(order.status)}
+                          color={getStatusColor(order.nombreEstado)}
                           size="small"
                         />
                       </TableCell>
@@ -129,6 +155,56 @@ const OrdenesUsuario = () => {
             onRowsPerPageChange={handleChangeRowsPerPage}
           />
         </Box>
+        <Dialog
+        open={dialogOpenDetalles}
+        onClose={handleCloseDialogDetalles}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Detalles de la Orden</DialogTitle>
+        <DialogContent>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Producto</TableCell>
+                  <TableCell>Cantidad</TableCell>
+                  <TableCell>Precio</TableCell>
+                  <TableCell>Total</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {ordenes
+                  .filter((orden) => orden.idorden === idOrden)
+                  .map((orden) => (
+                    <TableRow key={orden.idorden}>
+                      <TableCell>{orden.producto}</TableCell>
+                      <TableCell>{orden.cantidad}</TableCell>
+                      <TableCell>{orden.precio}</TableCell>
+                      <TableCell>{orden.total}</TableCell>
+                    </TableRow>
+                  ))}
+
+                <TableRow>
+                  <TableCell rowSpan={3} />
+                  <TableCell colSpan={2}>Total Detalles</TableCell>
+                  <TableCell align="right">
+                    Q
+                    {ordenes
+                      .filter((orden) => orden.idorden === idOrden)
+                      .reduce((total, orden) => total + orden.total, 0)}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialogDetalles} color="primary">
+            Cerrar
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       </div>
     </div>
   );
